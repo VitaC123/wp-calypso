e790c3635d5970570215079bdd7c241823bc17a3
Tugdual de Kerviler <dekervit@gmail.com>
2017-04-13 16:08:28 +0200

    Convert login WP_Error returned from the API to a valid translated message

--- client/state/login/actions.js
+++ client/state/login/actions.js
@@ -1,13 +1,28 @@
 /**
+ * External dependencies
+ */
+import { translate } from 'i18n-calypso';
+
+/**
  * Internal dependencies
  */
 import config from 'config';
 import request from 'superagent';
+import { get } from 'lodash';
 import {
 	LOGIN_REQUEST,
 	LOGIN_REQUEST_FAILURE,
 	LOGIN_REQUEST_SUCCESS,
 } from 'state/action-types';
+
+const loginErrorMessages = {
+	empty_password: translate( 'The password field is empty.' ),
+	empty_username: translate( 'The username field is empty.' ),
+	invalid_username: translate( 'Invalid username or password.' ),
+	incorrect_password: translate( 'Invalid username or password.' ),
+	unknown: translate( 'Invalid username or password.' ),
+	account_unactivated: translate( 'This account has not been activated. Please check your email for an activation link.' )
+};
 
 /**
  * Attempt to login a user.
@@ -37,12 +52,19 @@ export const loginUser = ( usernameOrEmail, password ) => dispatch => {
 				data: reponse.body,
 			} );
 		} ).catch( ( error ) => {
+			let errorMessage;
+			const errorKeys = get( error, 'response.body.data.errors' );
+			if ( errorKeys ) {
+				errorMessage = errorKeys.map( errorKey => loginErrorMessages[ errorKey ] ).join( ' ' );
+			} else {
+				errorMessage = error.message;
+			}
 			dispatch( {
 				type: LOGIN_REQUEST_FAILURE,
 				usernameOrEmail,
-				error: error.message
+				error: errorMessage
 			} );
 
-			return Promise.reject();
+			return Promise.reject( errorMessage );
 		} );
 };
